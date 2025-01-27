e8d0e279cb315d178dc2ed3d9b0129e544b58001
Zeke Piskulich <piskulichz@gmail.com>
2025-01-27 12:59:56 -0500

    Update analysis directory structure to include a 'data' subdirectory for trial outputs

--- rbfe_controller.py
+++ rbfe_controller.py
@@ -650,7 +650,7 @@ class RBFE_Analysis:
     def grab_data_lines(self):
         for edge in self.calculation.edges:
             for trial in self.trials:
-                analysis_dir = self.output_dir / edge.name / f"{trial}"
+                analysis_dir = self.output_dir / "data"/ edge.name / f"{trial}"
                 if not analysis_dir.exists():
                     analysis_dir.mkdir(parents=True, exist_ok=True)
                 line=f"edgembar-amber2dats.py -r {edge.com}/remt{trial}.log --odir={analysis_dir}/data $(ls {edge.com}/t{trial}/*ti.mdout)\n"
