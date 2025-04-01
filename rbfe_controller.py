98daa65b620f493e46a2b68a5517fd4ae49fd6e3
Zeke Piskulich <piskulichz@gmail.com>
2025-04-01 10:49:01 -0400

    Add distance restraint functionality and residue formatting

--- rbfe_controller.py
+++ rbfe_controller.py
@@ -4,6 +4,8 @@ import numpy as np
 import shutil
 import subprocess
 
+import MDAnalysis as mda
+
 class Calculation:
     def __init__(self, system):
         self.system = system
@@ -783,7 +785,60 @@ for edge in edges:
             print("Exception: No matching lambda schedules found.")
             return False
 
-    
+def format_residues(resids):
+    """ Format a list of residues into a string of ranges."""
+    ranges = []
+    start = resids[0]
+    end = resids[0]
+
+    for resid in resids[1:]:
+        if resid == end + 1:
+            end = resid
+        else:
+            if start == end:
+                ranges.append(f"{start}")
+            else:
+                ranges.append(f"{start}-{end}")
+            start = resid
+            end = resid
+
+    if start == end:
+        ranges.append(f"{start}")
+    else:
+        ranges.append(f"{start}-{end}")
+
+    return ",".join(ranges)
+
+
+def get_sel(mola, molb, mask, systemname="1Y27_rms0"):
+    """ Get the selection of residues for a given edge."""
+    # Load the topology and coordinate files
+    parm7_file = f"{systemname}/unified/run/{mola}~{molb}/com/unisc.parm7"
+    rst7_file = f"avRMSD/outputs/av_lig_tgt_{mola}~{molb}.rst7"
+    u = mda.Universe(parm7_file, rst7_file, topology_format="PARM7", format="RESTRT")
+    t1, t2 = u.select_atoms("resid 1"), u.select_atoms("resid 2")
+    # Define the selection criteria
+    selection = mask
+
+    # Select the residues within 3 Ångströms of residues 1 and 2
+    selected_atoms = u.select_atoms(selection)
+
+    selected_residues = selected_atoms.residues.resids
+    # Sort and find contiguous ranges
+    selected_residues = np.sort(np.unique(selected_residues))
+
+    return set(selected_residues)
+
+def GenDistRestraint(distance, reference_system):
+    edge_set = []
+    for file in glob("avRMSD/outputs/av_lig_tgt_*~*.rst7"):
+        ename = file.split("av_lig_tgt_")[-1]
+        edge = ename.split(".")[0]
+        mola, molb = edge.split("~")
+        mask = f'element P and not (around {distance} resid 1 or around {distance} resid 2)'
+        edge_set.append(get_sel(mola,molb,mask, systemname=reference_system))
+    shared_elements = set.intersection(*edge_set)
+    print(format_residues(list(shared_elements)))
 
 
 
@@ -805,6 +860,7 @@ if __name__ == "__main__":
     parser.add_argument("--ntrials",            default=3,                      type=int, help="The number of trials to copy")
     parser.add_argument("--endpoints_only",     default="False",                type=str, help="Only change the endpoints")
     parser.add_argument("--nlambda",           default=16,                      type=str, help="Optimize the lambda schedule")
+    parser.add_argument("--distance",          default=6,                      type=int, help="The distance to use for the distance restraints")
     args = parser.parse_args()
 
     if args.toolkit_bin is not None:
@@ -872,6 +928,12 @@ if __name__ == "__main__":
         rmsd = RMSRestraints(args.reference)
         print("Apply Reference Structures")
         rmsd.ApplyReferenceToSystem(args.modify)
+
+    if args.mode == "dist_restraints":
+        if args.reference is None:
+            raise ValueError("Must provide a reference system for the distance restraints.")
+        GenDistRestraint(args.distance, args.reference)
+
     
     if args.mode == "analysis":
         if args.reference is None:
